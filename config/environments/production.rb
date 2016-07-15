0388e9f7bf479ca892a9c04646faa59aead315f8
Jaewoo Park <jp3242@stern.nyu.edu>
2016-07-15 02:03:43 +0000

    Added the devise gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Nomster::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.default_url_options = { host: 'nomster-jaewoo-park.herokuapp.com' }
 gem 'rails_12factor', group: :production
 
   # Code is not reloaded between requests.
